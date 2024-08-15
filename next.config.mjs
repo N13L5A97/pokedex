35fb52c51facf0ff51304c50eb685d753a0fbe24
Niels Aling <niels.aling@hva.nl>
2024-08-15 14:39:51 +0200

    setup pokedex

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+        images: {
+          remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'www.pokemon.com',
+            },
+          ],
+        },
+};
 
 export default nextConfig;
