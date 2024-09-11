046d311cd901b326c875786ea2e33a7003bce084
Sofyan Bouazzati <sofyan@avocado-media.nl>
2024-09-11 15:54:06 +0200

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Footer() {
+
+    return (
+        <div className="p-4 text-black bg-white">
+            <p>This Pokédex is made by Niels</p>
+        </div>
+    );
+}
