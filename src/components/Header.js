5552bb62982f4611be797d4d9cf9330e9d2387ab
Sofyan Bouazzati <sofyan@avocado-media.nl>
2024-09-11 15:43:12 +0200

    made link out logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header({ onSearch, onFilter, sortOption, setSortOption }) {
 
     return (
-        <div className="flex flex-col items-center pt-10">
+        <div>
+            <Link href="/" className="flex flex-col items-center pt-10">
                 <Image src="/pokeball.png" alt="Pokeball" width={100} height={100} />
                 <h1 className="mt-5 mb-10 text-4xl font-bold text-center text-white">Pokédex</h1>
+            </Link>
         </div>
     );
 }
