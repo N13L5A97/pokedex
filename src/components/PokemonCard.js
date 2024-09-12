061aa25695c6ebc1de4b9bf67ae4e5e7cb4b217e
Sofyan Bouazzati <sofyan@avocado-media.nl>
2024-09-12 14:30:25 +0200

    weird error image fix

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -9,8 +9,7 @@ export default function Pokemon(pokemon) {
     // make string into int to check if index is above 999
     const indexInt = parseInt(index, 10);
 
-    console.log(indexInt);
-
+    // console.log(indexInt);
     if (indexInt >= 1000) {
         pokeIndex = index;
         // console.log("its higher then 999")
@@ -21,13 +20,14 @@ export default function Pokemon(pokemon) {
     return (
         <div className="relative flex-col items-center justify-center w-full p-4 text-black bg-white rounded-lg">
             <Link href={`/pokemon/${pokemon.name}`}>
-                {/* <Image
+                <Image
                     src={`https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/${pokeIndex}.png`}
                     alt={pokemon.name}
+                    // unoptimized
                     width={200}
                     height={200}
                     className="m-auto"
-                /> */}
+                />
                 <h2 className="text-xl font-bold capitalize">{pokemon.name}</h2>
                 <span className="absolute top-0 left-0 p-2 text-sm text-gray-500">#{pokeIndex}</span>
             </Link>
