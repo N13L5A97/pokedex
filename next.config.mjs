2b998fdd9c8a39bc4d27a32a8bc257fb473b0799
Sofyan Bouazzati <sofyan@avocado-media.nl>
2024-09-12 16:38:11 +0200

    add pathname to image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-        images: {
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'www.pokemon.com',
-            },
-          ],
-        },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.pokemon.com',
+        pathname: '/static-assets/content-assets/cms2/img/pokedex/full/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
